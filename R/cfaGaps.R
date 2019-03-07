a6bd2f85c79135a6cccb05230b289703ca87c1f3
Damian Betebenner <dbetebenner@nciea.org>
2019-03-07 16:32:45 -0500

    Tidying up package and bumping version number

--- R/cfaGaps.R
+++ R/cfaGaps.R
@@ -1,8 +1,31 @@
 `cfaGaps` <-
 function(
 	gap_data,
-	gap_groups=NULL) {
+	gap_groups=NULL,
+	output_format="PDF") {
 
-		print("On the way!")
+	if ("PNG" %in% output_format) {
+
+		print("PNG Plots Coming Soon!")
+
+	}
+
+	if ("PDF" %in% output_format) {
+
+		print("PDF Plots Coming Soon!")
+
+	}
+
+	if ("SVG" %in% output_format) {
+
+		print("SVG Plots Coming Soon!")
+
+	}
+
+	if ("PLOTLY" %in% output_format) {
+
+		print("Coming Soon!")
+
+	}
 
 } ### END cfaGaps

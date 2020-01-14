5034f24a06a71f241e07565d042ffb205afce92a
Damian Betebenner <dbetebenner@nciea.org>
2020-01-14 10:50:04 -0500

    Minor bug fixes to get package CHECK warnings and update to package webpage

--- R/cfaGaps.R
+++ R/cfaGaps.R
@@ -35,9 +35,9 @@ function(
 
 		# Create viewports
 
-		figure.vp <- viewport(layout = grid.layout(3, 3, widths = unit(c(0.8, 9.5, 2.7)*fig_width/13, rep("inches", 3)),
-							  heights = unit(c(0.2, 7.6, 0.8)*fig_height/8.5, rep("inches", 3))),
-							  gp=gpar(cex=fig_width/13))
+		figure.vp <- viewport(layout = grid.layout(3, 3, widths = unit(c(0.8, 9.5, 2.7)*gap_plot_fig_width/13, rep("inches", 3)),
+							  heights = unit(c(0.2, 7.6, 0.8)*gap_plot_fig_height/8.5, rep("inches", 3))),
+							  gp=gpar(cex=gap_plot_fig_width/13))
 
 	  	title.vp <- viewport(name="title.vp",
 		                      layout.pos.row=1, layout.pos.col=1:3,
@@ -53,24 +53,24 @@ function(
 
 	    vaxis.vp <- viewport(name="vaxis.vp",
 	                          layout.pos.row=2, layout.pos.col=1,
-	                          xscale=c(0,1),
-	                          yscale=extendrange(bubble_plot_configs.BUBBLE_Y_TICKS, f=0.025),
+#	                          xscale=c(0,1),
+#	                          yscale=extendrange(bubble_plot_configs.BUBBLE_Y_TICKS, f=0.025),
 	                          gp=gpar(fill="transparent", cex=1.2))
 
  	    chart.vp <- viewport(name="chart.vp",
   		                  	  layout.pos.row=2, layout.pos.col=2,
-  		                  	  xscale=extendrange(bubble_plot_configs.BUBBLE_X_TICKS, f=0.025),
-  		                  	  yscale=extendrange(bubble_plot_configs.BUBBLE_Y_TICKS, f=0.025),
+#  		                  	  xscale=extendrange(bubble_plot_configs.BUBBLE_X_TICKS, f=0.025),
+#  		                  	  yscale=extendrange(bubble_plot_configs.BUBBLE_Y_TICKS, f=0.025),
   		                  	  gp=gpar(fill="transparent"))
 
   		haxis.vp <- viewport(name="haxis.vp",
   		                      layout.pos.row=3, layout.pos.col=2,
-  		                      xscale=extendrange(bubble_plot_configs.BUBBLE_X_TICKS, f=0.025),
-  		                      yscale=c(0,1),
+# 		                      xscale=extendrange(bubble_plot_configs.BUBBLE_X_TICKS, f=0.025),
+#		                      yscale=c(0,1),
   		                      gp=gpar(fill="transparent", cex=1.2))
 
 
-		pdf(file=file.path.and.name, width=fig.width, height=8.5, bg=format.colors.background, version="1.4")
+		pdf(file=file.path.and.name, width=gap_plot_fig_width, height=gap_plot_fig_height)
 
 
 		# Push figure.vp

273809efc57806ca23fc79da8e8c14579871e892
Damian Betebenner <dbetebenner@nciea.org>
2021-06-15 12:34:16 -0400

    Adding to website updates

--- R/bootstrapSRS.R
+++ R/bootstrapSRS.R
@@ -9,8 +9,7 @@ function(summary_variable,
 
 	sample_data <- data.table(SUMMARY_VARIABLE=summary_variable, STRATA_VARIABLE=as.character(strata_variable))
 	if (is.null(sample_size)) sample_size=nrow(sample_data)
-    if (length(population_strata_proportions) != uniqueN(sample_data[['STRATA_VARIABLE']])) population_strata_proportions <- population_strata_proportions[unique(sample_data[['STRATA_VARIABLE']])]
-#	stopifnot(length(population_strata_proportions) == uniqueN(sample_data[['STRATA_VARIABLE']]))
+    if (length(population_strata_proportions[!is.na(population_strata_proportions)]) != uniqueN(sample_data[['STRATA_VARIABLE']])) population_strata_proportions <- population_strata_proportions[!is.na(population_strata_proportions)][unique(sample_data[['STRATA_VARIABLE']])]
 	tmp.replications <- ceiling(max((population_strata_proportions*sample_size)/table(sample_data[['STRATA_VARIABLE']])))
 	tmp.replicated_sample_data <- rbindlist(replicate(tmp.replications, return(sample_data), simplify=FALSE))
 	tmp.sample_statistic <- replicate(bootstrap_iterations, eval(parse(text=paste0(summary_statistic, "(tmp.replicated_sample_data[, .SD[sample(.N, ceiling(sample_size*population_strata_proportions[[unlist(.BY)]]))], keyby=STRATA_VARIABLE][['SUMMARY_VARIABLE']], na.rm=TRUE)"))))
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ cfaTools
 [![R-CMD-check](https://github.com/CenterForAssessment/cfaTools/workflows/R-CMD-check/badge.svg)](https://github.com/CenterForAssessment/cfaTools/actions)
 [![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/centerforassessment/cfaTools?branch=master&svg=true)](https://ci.appveyor.com/project/centerforassessment/cfaTools)
 [![CRAN_Status_Badge](http://www.r-pkg.org/badges/version/cfaTools)](http://cran.r-project.org/package=cfaTools)
-[![Development Version](https://img.shields.io/badge/devel-0.0--0.99-brightgreen.svg)](https://github.com/CenterForAssessment/cfaTools)
+[![Development Version](https://img.shields.io/badge/devel-0.0--1.0-brightgreen.svg)](https://github.com/CenterForAssessment/cfaTools)
 [![License](http://img.shields.io/badge/license-GPL%203-brightgreen.svg?style=flat)](https://github.com/CenterForAssessment/cfaTools/blob/master/LICENSE.md)
 
 

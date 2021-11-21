2087a0bfe01813997e682fd06db64042b3124ec2
Adam VanIwaarden <avaniwaarden@gmail.com>
2021-11-21 09:50:21 -0700

    Add VALID_CASE check and improve multiple shift variable and amount functionality

--- R/getShiftedValues.R
+++ R/getShiftedValues.R
@@ -16,12 +16,17 @@ function(
             res[]
     }
 
+    if ("VALID_CASE" %in% names(data_table)) valid_case <- "VALID_CASE" else valid_case <- NULL
     data_table[,FILTER_VARIABLE:=seq.int(dim(data_table)[1])]
-    data_table_complete <- completeDT(data_table[,c("FILTER_VARIABLE", shift_period, shift_group,  shift_variable), with=FALSE], cols=c(shift_period, shift_group))
-    setkeyv(data_table_complete, c(shift_group, shift_period))
-    data_table_complete[, paste(shift_variable, toupper(shift_type), shift_amount, sep="_") := shift(get(shift_variable), n=shift_amount, type=shift_type), by=shift_group]
+    data_table_complete <- completeDT(data_table[,c("FILTER_VARIABLE", shift_group, shift_period, valid_case, shift_variable), with=FALSE], cols=c(shift_period, shift_group))
+    if (!is.null(valid_case)) data_table_complete[is.na(VALID_CASE), VALID_CASE := "VALID_CASE"]
+    setkeyv(data_table_complete, c(shift_group, shift_period, valid_case))
+    shift_var_names <- paste(rep(shift_variable, each=length(shift_amount)), toupper(shift_type), shift_amount, sep="_")
+    data_table_complete[, (shift_var_names) := shift(.SD, n=shift_amount, type=shift_type), .SDcols=shift_variable, by=shift_group]
+    # data_table_complete[, paste(shift_variable, toupper(shift_type), shift_amount, sep="_") := shift(get(shift_variable), n=shift_amount, type=shift_type), by=shift_group]
     data_table_complete <- data_table_complete[!is.na(FILTER_VARIABLE)]
     setkeyv(data_table_complete, "FILTER_VARIABLE")
-    data_table[,paste(shift_variable, toupper(shift_type), shift_amount, sep="_") := data_table_complete[, paste(shift_variable, toupper(shift_type), shift_amount, sep="_"), with=FALSE]]
+    data_table[, (shift_var_names) := data_table_complete[, (shift_var_names), with=FALSE]]
+    # data_table[,paste(shift_variable, toupper(shift_type), shift_amount, sep="_") := data_table_complete[, paste(shift_variable, toupper(shift_type), shift_amount, sep="_"), with=FALSE]]
     return(data_table[,FILTER_VARIABLE:=NULL])
 } ### END getShiftedValues function

c287469a2b26bf1f835f226e871bc93fc0d5a74e
Damian Betebenner <dbetebenner@nciea.org>
2024-12-09 05:43:38 -0500

    Update of startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,18 +2,45 @@
 function(libname, pkgname) {
 }
 
-.onAttach <- function(libname, pkgname) {
+`.onAttach` <- function(libname, pkgname) {
     if (interactive()) {
+		# Utility function 
+        get_dev_version <- function(package) {
+            url <- paste0("https://raw.githubusercontent.com/CenterForAssessment/", package, "/refs/heads/master/DESCRIPTION")
+            tryCatch({
+                lines <- readLines(url, warn = FALSE)
+                version_line <- grep("^Version:", lines, value = TRUE)
+                if (length(version_line) > 0) {
+                    return(cyan("v", strsplit(version_line, ": ")[[1]][2], sep=""))
+                } else {
+                    return(red("Not Available"))
+                }
+            }, error = function(e) {
+                return(red("Not Available"))
+            }, warning = function(w) {
+                return(red("Not Available"))
+            })
+        }
+
         # Extract version information
-        version <- utils::packageVersion("cfaTools")
+        installed.version <- utils::packageDescription("cfaTools")[['Version']]
+        cran.version <- tryCatch(
+            green("v", pkgsearch::cran_package("cfaTools")[['Version']], sep=""),
+            error = function(e) red("Not Available"),
+            warning = function(w) red("Not Available"))
+        dev.version <- get_dev_version("cfaTools")
 
         # Define a friendly startup message
-	message_text <- paste0(
-	    magenta(bold("\uD83C\uDF89 cfaTools v", version)), " - ", toOrdinal::toOrdinalDate("2024-12-3"), "\n",
-	    "\U1F4A1 Tip: ", magenta(bold("> help(\"cfaTools\")")), "\n",
-	    "\U1F310 Docs: ", magenta(bold("https://centerforassessment.github.io/cfaTools")), "\n",
-	    "\u2728 Happy cfaTooling!"
-	)
+		message_text <- paste0(
+		    magenta(bold("\uD83C\uDF89 cfaTools v", installed.version, sep="")), " - ", toOrdinal::toOrdinalDate("2024-12-8"), "\n",
+			strrep("\u2501", 40), "\n",
+    	    bold("\U1F4E6 CRAN: "), cran.version, "\n",
+    	    bold("\U1F527 Dev: "), dev.version, "\n",
+			strrep("\u2501", 40), "\n",
+		    "\U1F4A1 Tip: ", magenta(bold("> help(package=\"cfaTools\")")), "\n",
+		    "\U1F310 Docs: ", magenta(bold("https://centerforassessment.github.io/cfaTools")), "\n",
+			strrep("\u2501", 40), "\n",
+		    "\u2728 Happy cfaTooling!")
 
         # Display the startup message
         packageStartupMessage(message_text)
